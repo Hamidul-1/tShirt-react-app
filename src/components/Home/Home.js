7994602f61ab0072ac5b9fd103136058d674bffc
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-01 12:44:32 +0600

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTShirts from '../../hooks/useTShirts';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,18 +6,41 @@ import './Home.css';
 
 const Home = () => {
     const [tShirts, setTShirts] = useTShirts();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (selectedItem) => {
+        const exist = cart.find(tShirt => tShirt._id === selectedItem._id);
+        if (!exist) {
+            const newCart = [...cart, selectedItem];
+            setCart(newCart);
+        }
+        else{
+            alert('item already added');
+        }
+
+    }
+
+    const handleRemoveFromCart = (selectedItem) => {
+        const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
+        setCart(rest);
+    }
+
     return (
         <div className='home-container'>
             <div className="tshirt-container">
                 {
-                    tShirts.map(tShirt =><TShirt
+                    tShirts.map(tShirt => <TShirt
                         key={tShirt._id}
                         tShirt={tShirt}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    cart={cart}
+                ></Cart>
             </div>
         </div>
     );
--- src/components/TShirt/TShirt.js
+++ src/components/TShirt/TShirt.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './TShirt.css';
 
-const TShirt = (props) => {
-    const {name, picture, price} = props.tShirt;
+const TShirt = ({handleAddToCart, tShirt}) => {
+    const {name, picture, price} = tShirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
             <h4>{name}</h4>
             <p>Price: ${price}</p>
-            <button>Add to cart</button>
+            <button onClick={()=> handleAddToCart(tShirt)}>Add to cart</button>
         </div>
     );
 };
